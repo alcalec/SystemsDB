bcbdf455f77afbaf7b5e1ca030e439772fd37e55
alcalec <aalcocer@csi.edu>
2026-08-23 22:58:43 -0600

    Update Homework.sql
    includes notes

--- Week1/Homework.sql
+++ Week1/Homework.sql
@@ -1,1 +1,10 @@
 select current_date, current_database(), version(), 'Hello, SQL!';
+/*
+- Use Create Database (name); command to create a new database
+- Use Create Table (name); command
+  * bigserial is an integer that auto-increments- Postgre specific
+  * varchar is a text column, includes parentheses for amount of characters
+- Semicolons signal the end of command, part of the ANSI SQL standard, good practice as well
+- date data type allows a date to be typed and shown
+- salary date type is numeric type to be inserted and shown
+*/
